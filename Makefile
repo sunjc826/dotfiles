b757e72f5200aa3757506662a4cae7403ec919f3
Sun Jia Cheng <sunjc826@gmail.com>
2024-06-01 13:34:17 +0800

    Refactor into Makefile

--- Makefile
+++ Makefile
@@ -1,9 +1,29 @@
-.PHONY: clean
-clean:
-	rm install_flag
+SHELL := /bin/bash
+SCRIPT_FILES := .bashrc .lessfilter
+FILES := $(SCRIPT_FILES) .gdbinit .inputrc .shellcheckrc .vimrc
+SCRIPT_FILE_FLAGS := $(patsubst %,%_install_flag,$(SCRIPT_FILES))
+FILE_FLAGS := $(patsubst %,%_install_flag,$(FILES))
+
+.PHONY: clean_install
+# TODO: uninstall scripts
+clean_install: 
+	-rm install_flag $(FILE_FLAGS)
 
 .PHONY: install
-install: install_flag
-install_flag:
+install: $(FILE_FLAGS)
+$(FILE_FLAGS): %_install_flag : %
+$(SCRIPT_FILE_FLAGS): %_install_flag : %
+	. install.sh && append_sh_source_file '$<'
 	touch $@
-	install.sh
+.gdbinit_install_flag:
+	. install.sh && gdbinit_install
+	touch $@
+.inputrc_install_flag:
+	. install.sh && append_source_file '$$include' .inputrc
+	touch $@
+.shellcheckrc_install_flag:
+	. install.sh && ln -s '$(realpath .shellcheckrc)' "$$HOME"/.shellcheckrc
+	touch $@ 
+.vimrc_install_flag:
+	. install.sh && append_source_file so .vimrc
+	touch $@
--- install.sh
+++ install.sh
@@ -1,19 +1,36 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
-pushd "$SCRIPT_DIR" || exit 1
-set -x
-echo 'source '"${SCRIPT_DIR}"/.bashrc >> "$HOME"/.bashrc
-# shellcheck disable=SC2016
-echo '$include '"${SCRIPT_DIR}"/.inputrc >> "$HOME"/.inputrc
-echo 'source '"${SCRIPT_DIR}"/.lessfilter >> "$HOME"/.lessfilter
-GDBINIT=$HOME/.config/gdb/gdbinit
-if test -e "$HOME"/.gdbinit; then
-    GDBINIT="$HOME"/.gdbinit
-fi
-mkdir -p "$(dirname "$GDBINIT")"
-echo 'source '"${SCRIPT_DIR}"/.gdbinit >> "$GDBINIT"
-ln -s "$(realpath .shellcheckrc)" "$HOME"/.shellcheckrc
-echo 'so '"${SCRIPT_DIR}"/.vimrc >> "$HOME"/.vimrc
-set +x
-popd
+cd "$SCRIPT_DIR" || exit 1
+
+append_source_file() {
+    local source_keyword
+    source_keyword=$1
+    local src_filepath # relative
+    src_filepath=$2
+    local dest_filepath # relative to $HOME, can be a directory as well
+    dest_filepath=${3:-${src_filepath}}
+    echo "$source_keyword"' '"${SCRIPT_DIR}"/"$src_filepath" >> "$HOME"/"$dest_filepath"
+}
+
+append_sh_source_file() {
+    local src_filepath # relative
+    src_filepath=$1
+    local dest_filepath # relative to $HOME
+    dest_filepath=${2:-${src_filepath}}
+    if ! test -e "$HOME"/"$dest_filepath"; then
+        echo '#!/usr/bin/env bash' >> "$HOME"/"$dest_filepath"
+        chmod +x "$HOME"/"$dest_filepath"
+    fi
+    append_source_file . "$src_filepath" "$dest_filepath" 
+}
+
+gdbinit_install() {
+    local gdbinit
+    gdbinit=.config/gdb/gdbinit
+    if test -e "$HOME"/.gdbinit; then
+        gdbinit=.gdbinit
+    fi
+    mkdir -p "$(dirname "$HOME"/"$gdbinit")"
+    append_source_file source .gdbinit "$gdbinit"
+}
